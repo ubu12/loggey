677e17cfee8d30d72fbf6f59f5e474f21202366e
Zay Jacobs <admin@ambrosia.club>
2021-09-09 09:51:33 +1000

    Update Loggey.cpp

--- Loggey.cpp
+++ Loggey.cpp
@@ -1,4 +1,4 @@
-﻿// Loggey example.
+// Loggey example.
 // written by: Spedzay
 
 #include "Loggey.hpp"
@@ -11,19 +11,19 @@ logType MESSAGE = { colors::light_blue,
 
 
 int main() {
-	MESSAGE.prefix = "Init";
-	MESSAGE.flags = logTypeFlags::addPrefixEncasing;
-	loggey_settings::textColor = colors::magenta;
+    MESSAGE.prefix = "Init";
+    MESSAGE.flags = logTypeFlags::addPrefixEncasing;
+    loggey_settings::textColor = colors::magenta;
 
-	log(MESSAGE, "Loggey, the single header C++17 logging library");
+    log(MESSAGE, "Loggey, the single header C++17 logging library");
 
-	loggey_settings::textColor = colors::white;
-	MESSAGE.flags = logTypeFlags::addPlus;
-	MESSAGE.prefix = "LOG";
+    loggey_settings::textColor = colors::white;
+    MESSAGE.flags = logTypeFlags::addPlus;
+    MESSAGE.prefix = "LOG";
 
-	log(MESSAGE, "Loggey is written by Spedzay");
-	log(MESSAGE, "Any contributions are welcome!");
-	log(MESSAGE, "Loggey is indev as of right now.");
+    log(MESSAGE, "Loggey is written by Spedzay");
+    log(MESSAGE, "Any contributions are welcome!");
+    log(MESSAGE, "Loggey is indev as of right now.");
 
     std::cin.get();
     return 0;
