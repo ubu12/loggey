3fec1d5997d3c1272de527ea59f5115823e24e43
Zay Jacobs <admin@ambrosia.club>
2021-09-09 09:50:58 +1000

    FIX: Example: More simple example

--- Loggey.cpp
+++ Loggey.cpp
@@ -3,35 +3,27 @@
 
 #include "Loggey.hpp"
 
-#include <iostream>
-
 using namespace std;
 using namespace loggey;
 
-logType ALERT = { colors::red,
-	"Alert", logTypeFlags::addPlus };
-
-logType WARNING = { colors::orange,
-	"Warning", logTypeFlags::addPlus };
-
 logType MESSAGE = { colors::light_blue,
-	"LOG", logTypeFlags::addPlus };
+	"LOG", logTypeFlags::addEncasing };
 
 
 int main() {
-	WARNING.prefix = "Init";
-	WARNING.flags = logTypeFlags::addPrefixEncasing;
+	MESSAGE.prefix = "Init";
+	MESSAGE.flags = logTypeFlags::addPrefixEncasing;
 	loggey_settings::textColor = colors::magenta;
 
-	log(WARNING, "Loggey, the single header C++17 logging library");
+	log(MESSAGE, "Loggey, the single header C++17 logging library");
 
 	loggey_settings::textColor = colors::white;
-	WARNING.flags = logTypeFlags::addPlus;
-	WARNING.prefix = "Warning";
+	MESSAGE.flags = logTypeFlags::addPlus;
+	MESSAGE.prefix = "LOG";
 
 	log(MESSAGE, "Loggey is written by Spedzay");
 	log(MESSAGE, "Any contributions are welcome!");
-	log(MESSAGE, "Its under MIT License, tho its really bad.");
+	log(MESSAGE, "Loggey is indev as of right now.");
 
     std::cin.get();
     return 0;
